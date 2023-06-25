8c1c4aecd252772d6497a7e6661161b6fa9f8030
paracetamol31 <ilya.hotchenkov@gmail.com>
2023-06-25 17:01:14 +0300

    Настроил шаблонный webpack.config

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+const App = () => {
+    return (
+        <>
+            <span>"webpack"</span>
+            <main>
+                <div className="img"></div>
+                <span>"Hello World!"</span>
+            </main>
+
+            <footer>
+                <div className="image"></div>
+            </footer>
+        </>
+    )
+}
+
+export { App };
